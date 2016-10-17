f24bf9cad21d2dda179252465a5e2f2f99be7407
maov <marcusoverheu@gmail.com>
2016-10-18 00:12:57 +0200

    start adding routes

--- server.py
+++ server.py
@@ -3,32 +3,61 @@ import socket
 import sys
 import common as cc
 import logging
+import subprocess
+
 
 entry_start = '### START AUTOMATIC GENERATED ENTRIES ###\n'
 entry_stop = '### END AUTOMATIC GENERATED ENTRIES ###\n'
 
-#read until start tag copy all elements untill end tag, and return dict, last entryfile
-#if no start_tag create start and end tag
-def open_host_file():
+INTERFACE="eth0"
+COMMON_PREFIX='fdee:f345:543f'
+
+#read until start tag copy all elements untill end tag, save file and return dict, last entryfile
+def open_host_file(host_name=None, ipv6=None):
     lines = []
     with open('/etc/hosts','r') as host_file:
         lines = host_file.readlines()
     if entry_start not in lines :
         ## create start and stop entries
-        with open('/etc/hosts','a') as host_file:
-            host_file.write(entry_start)
-            host_file.write('\n')
-            host_file.write(entry_stop)
-            return {}
-    else :
-        ## Make list of lines with entries
-        ## return list
-        print('lalal')
-        start_idx = lines.index(entry_start)
-        stop_idx = lines.index(entrystop)
-        entries = lines[start_idx + 1 : stop_idx]
-        key_value = [e.split(' ',1).reverse() for e in entries if ' ' in e]
-        return dict(key_value)
+        lines.append(entr_start)
+        lines.append(entry_stop)
+    start_idx = lines.index(entry_start)
+    stop_idx = lines.index(entry_stop)
+    new_line = '' if host_name is None else '{0} {1}'.format(ipv6, host_name)
+    lines.insert(start_idx + 1, new_line)
+    entries = lines[start_idx + 1 : stop_idx]
+    key_value = [e.split(' ',1).reverse() for e in entries if ' ' in e]
+    with open('/etc/hosts','w') as host_file :
+        host_file.write("".join(lines))
+    return dict(key_value)
+
+
+def setup_route(prefix=None, gateway=None):
+    #Get routes
+    cmd = 'ip -6 route'
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    (output, err) = proc.communicate()
+    prefix_gateway = {}
+    if err is not None:
+        raise exception("something went wrong calling {0} \n{1}".format(cmd, err))
+    else: 
+        routes = output.decode('utf-8').split("\n")
+        common_routes = routes.startswith(COMMON_PREFIX).split(' ')
+        prefix_gateway = dict([(e[0],e[3]) for e in common_routes])
+        print(routes)
+    #what to do when ip/routes change, remove previous entry?!?!?
+    if prefix is not None AND prefix not IN prefix_gateway:
+        #add route
+        route_cmd = 'Adding, ip -6 route add {0} via {1}'.format(prefix, gateway)
+        print(route_cmd)
+        proc2 = subprocess.Popen(route_cmd, shell=True, stdout=subprocess.PIPE)
+        (output,error) = proc2.communicate()
+        if error is not None:
+            raise exception("something went wrong calling {0} \n{1}".format(cmd, error))
+        prefix_gateway[prefix] = gateway
+    return prefix_gateway
+
+setup_route()
 
 print(socket.gethostname())
 
@@ -44,6 +73,8 @@ sock.bind((server_address))
 
 open_host_file()
 
+
+
 try : 
     while True:
         print(hosts)
@@ -51,6 +82,8 @@ try :
         data, sender = sock.recvfrom(1500)
         print (str(sender) + '  ' + data.decode('utf-8'))
         data_spl = data.decode('utf-8').split(';')
+        #get device that reveived multicast???
+
         #Check hash hasnt been used yet
         #for lala in hosts where key <> key and prefix  = prefix
         #throw warning if its the case
